ca5a861583a8580de6028efb6a9e90af29ff336a
xieyao <754813200@qq.com>
2023-07-10 15:42:20 +0800

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * prettier options
+ * @description
+ * [prettier rules](https://prettier.io/docs/en/options.html)
+ */
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'always',
+  semi: true,
+  endOfLine: 'auto',
+  trailingComma: 'none',
+  printWidth: 100
+};
---
+++ src/components/Loading/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="loading-box">
+    <div class="loading">
+      loading
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+};
+</script>
+
+<style lang="scss" scoped>
+.loading-box {
+  position: relative;
+}
+</style>
